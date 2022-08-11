d459f6a71e9585a668c9025c88562250e2e68636
fenghuabin <fenghuabin@711.com>
2022-08-11 14:16:08 +0800

    add springboot-schedule-task project

---
+++ java-codes/springboot-schedule-task/annotation-based/src/main/java/com/bfh/AnnotationBasedMain.java
@@ -0,0 +1,35 @@
+package com.bfh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * @author benfeihu
+ */
+@SpringBootApplication
+@EnableScheduling
+@Slf4j
+public class AnnotationBasedMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationBasedMain.class, args);
+    }
+
+//    @Scheduled(cron = "*/5 * * * * ?")  // 每隔 5s 执行一次
+//    public void sayHello() {
+//        log.info("hello");
+//    }
+
+    @Scheduled(
+            fixedDelay = 2000    // 在上一个任务执行结束后，间隔2s再执行下一个
+            ,initialDelay = 3000  // 第一次启动间隔 3s
+    )
+    void fixedDelay() throws InterruptedException {
+        log.info("fixed Delay start");
+        Thread.sleep(5000);
+        log.info("fixed Delay end");
+    }
+
+}
---
+++ java-codes/springboot-schedule-task/annotation-based/src/main/java/com/bfh/Task1.java
@@ -0,0 +1,17 @@
+package com.bfh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author benfeihu
+ */
+@Component
+@Slf4j
+public class Task1 {
+//    @Scheduled(cron = "*/3 * * * * ?")
+//    private void sayWorld() {
+//        log.info("world");
+//    }
+}
