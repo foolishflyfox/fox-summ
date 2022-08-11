ea3e7af51da6b401c4cbd0cfbd736e30d79ae6c3
fenghuabin <fenghuabin@711.com>
2022-08-11 11:15:03 +0800

    nacos python service register

--- java-codes/nacos/nacos-python-demo/python-src/nacos_service.py
+++ java-codes/nacos/nacos-python-demo/python-src/nacos_service.py
@@ -2,7 +2,6 @@ import threading
 import requests
 import time
 
-
 # 自定义服务的信息
 # class CustomService:
 #     def __init__(self, name, ip, port):
@@ -16,12 +15,14 @@ import time
 # 2. 通过 start 启动服务，NacosService 会自动向注册中心暴露服务并维持心跳
 class NacosService:
     # 请求地址
-    reqPath = "/nacos/v1/ns/instance"
+    instancePath = "/nacos/v1/ns/instance"
+    instanceBeatPath = "/nacos/v1/ns/instance/beat"
 
     def __init__(self, nacosIP, nacosPort):
         self.ip = nacosIP
         self.port = nacosPort
-        self.serviceUrls = []
+        self.instanceUrls = []
+        self.instanceBeatUrls = []
         # 心跳周期，单位 s
         self.heatBeat = 5
 
@@ -33,23 +34,20 @@ class NacosService:
 
     def addService(self, serviceName, serviceIP, servicePort):
         # customService = CustomService(serviceName, serviceIP, servicePort)
-        self.serviceUrls.append(self.generateNacosUrl(serviceName, serviceIP, servicePort))
-
-    def generateNacosUrl(self, serviceName, serviceIP, servicePort):
-        return "http://{}:{}{}?serviceName={}&ip={}&port={}".format(
-            self.ip, self.port, NacosService.reqPath,
-            serviceName, serviceIP, servicePort
-        )
+        self.instanceUrls.append("http://{}:{}{}?serviceName={}&ip={}&port={}".format(
+            self.ip, self.port, NacosService.instancePath, serviceName, serviceIP, servicePort))
+        self.instanceBeatUrls.append("http://{}:{}{}?serviceName={}&ip={}&port={}".format(
+            self.ip, self.port, NacosService.instanceBeatPath, serviceName, serviceIP, servicePort))
     
     def keepAliveProcess(self):
         while True:
-            for url in self.serviceUrls:
+            for url in self.instanceBeatUrls:
                 res = requests.put(url)
                 print("心跳 {} 状态: {}".format(url, res.status_code))
                 time.sleep(self.heatBeat)
     
     def start(self):
-        for url in self.serviceUrls:
+        for url in self.instanceUrls:
             res = requests.post(url)
             print("注册 {} 结果为 {}".format(url, res.status_code))
         threading.Timer(self.heatBeat, self.keepAliveProcess).start()
