b60b6e9d790f3356e82d0126a41214f48659fcad
fenghuabin <fenghuabin@711.com>
2022-09-16 08:02:49 +0800

    add webpack.config.js demo

---
+++ config-demos/webpack.config.js
@@ -0,0 +1,14 @@
+// webpack 的常用配置
+const path = require("path");
+exports.mode = "development"; // 设置开发模式 production / development
+exports.entry = path.join(__dirname, "src/index1.js"); // 设置入口文件
+exports.output = {
+  path: path.join(__dirname, "bundle"), // 设置输出目录
+  filename: "m.js", // 设置输出文件名
+};
+exports.devServer = {
+  static: "./", // 供 webpack-dev-server 使用(webpack server 命令)
+};
+
+
+
