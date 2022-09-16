cdb105222239fe159a87656f867a28789a941613
fenghuabin <fenghuabin@711.com>
2022-09-16 08:05:22 +0800

    update webpack.config.js demo

--- config-demos/webpack.config.js
+++ config-demos/webpack.config.js
@@ -1,4 +1,4 @@
-// webpack 的常用配置
+// webpack 打包常用配置
 const path = require("path");
 exports.mode = "development"; // 设置开发模式 production / development
 exports.entry = path.join(__dirname, "src/index1.js"); // 设置入口文件
@@ -7,8 +7,6 @@ exports.output = {
   filename: "m.js", // 设置输出文件名
 };
 exports.devServer = {
-  static: "./", // 供 webpack-dev-server 使用(webpack server 命令)
+  static: "./", // 供 webpack-dev-server 使用(webpack server 命令),使之支持静态文件访问
 };
-
-
 
